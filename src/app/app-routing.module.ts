6a5e5e272db544a6be39d3a08fa2134334aa16c2
avendeep <pradeepg63@gmail.com>
2022-05-10 13:39:30 +0530

    nested & guarding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,45 @@
 import { NgModule } from "@angular/core";
-import { Routes,RouterModule } from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
+import { AuthGruard } from "./auth-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { canDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 
 const appRoutes: Routes = [
-    { path: "", component: HomeComponent },
-    {
-      path: "users",
-      component: UsersComponent,
-      children: [{ path: ":id/:name", component: UserComponent }],
-    },
-    {
-      path: "servers",
-      component: ServersComponent,
-      children: [
-        { path: ":id", component: ServerComponent },
-        { path: ":id/edit", component: EditServerComponent },
-      ],
-    },
-    {path:'404', component:PageNotFoundComponent},
-    {path:'**', redirectTo:'/404'},
-  ];
-
+  { path: "", component: HomeComponent },
+  {
+    path: "users",
+    component: UsersComponent,
+    children: [{ path: ":id/:name", component: UserComponent }],
+  },
+  {
+    path: "servers",
+    // canActivate:[AuthGruard], //Protects Parent & child route
+    canActivateChild: [AuthGruard], // Protects only child route
+    component: ServersComponent,
+    children: [
+      { path: ":id", component: ServerComponent, resolve: {server:ServerResolver}},
+      {
+        path: ":id/edit",
+        component: EditServerComponent,
+        canDeactivate: [canDeactivateGuard],
+      },
+    ],
+  },
+  //{ path: "404", component: PageNotFoundComponent },
+  { path: "404", component: ErrorPageComponent, data:{message:'Page not found!'} },
+  { path: "**", redirectTo: "/404" },
+];
 
 @NgModule({
-   imports:[
-    RouterModule.forRoot(appRoutes)
-   ],
-   exports:[RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule{
-
-}
+export class AppRoutingModule {}
--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Observable } from "rxjs";
 
 import { ServersService } from "../servers.service";
+import { canComponentDeativate } from "./can-deactivate-guard.service";
 
 @Component({
   selector: "app-edit-server",
   templateUrl: "./edit-server.component.html",
   styleUrls: ["./edit-server.component.css"],
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, canComponentDeativate {
   server: { id: number; name: string; status: string };
   serverName = "";
   serverStatus = "";
   allowEdit = false;
+  changesSaved = false
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -27,8 +31,15 @@ export class EditServerComponent implements OnInit {
       this.allowEdit = queryParams['allowEdit'] === '1'? true:false
     });
     this.route.fragment.subscribe();
+    
+     const id = +this.route.snapshot.params['id']
+    this.server = this.serversService.getServer(id);
+    
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params["id"]);
+    });
 
-    this.server = this.serversService.getServer(1);
+
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
@@ -38,5 +49,19 @@ export class EditServerComponent implements OnInit {
       name: this.serverName,
       status: this.serverStatus,
     });
+    this.changesSaved = true;
+    this.router.navigate(['../'],{relativeTo: this.route})
+  }
+
+  //canDeactivate: () => boolean | Observable<boolean> | Promise<boolean>;
+  canDeactivate():Observable<boolean> | Promise<boolean> | boolean {
+    if(!this.allowEdit){
+      return true
+    }
+    if(this.serverName !== this.server.name || this.serverStatus !== this.server.status && !this.changesSaved ){
+       return confirm('Do you want to discard the unsaved changes?')
+    }else{
+      return true;
+    }
   }
 }
